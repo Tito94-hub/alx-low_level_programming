22b5336a70dcbc32de571b93f7183b05f1ba46a1
Your Name <you@example.com>
2023-09-08 11:30:13 -0700

    positive anything

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,16 +13,16 @@ int main(void)
 {
 	int n;
 
-	srand(time(0));
+	srand(time(0))i;
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 if (n > 0)
 	printf("%d is positive", n);
 else if (n == 0)
-        printf("%d is zero", n);
+	printf("%d is zero", n);
 else
 	printf("%d is negative", n);
 printf("\n");
 
-        return (0);
+return (0);
 }
