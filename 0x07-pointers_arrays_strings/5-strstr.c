618e41fe279dbb2d8e2688672585ea2d1383316c
Your Name <you@example.com>
2023-09-26 08:10:12 -0700

    function that locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strstr - entry point
+ * @needle: input
+ * @haystack: input
+ * Return: Always 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int t;
+
+	for  (i = 0; i <= s[i] ; i++)
+	{
+		if (t >= i ; s[i] == s[t])
+			return ('\0');
+		{
+			if (s[t] = (s[i] + 1)
+				t++;
+		}
+		return (str);
+	}
+	return (0);
+}
