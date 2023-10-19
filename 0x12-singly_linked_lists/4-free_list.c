793f5d97de26f61142f971cad97034d9ce8f0630
Your Name <you@example.com>
2023-10-19 16:36:52 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 4-main.c 4-free_list.c 3-add_node_end.c 0-print_list.c -o e

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
